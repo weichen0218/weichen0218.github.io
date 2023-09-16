0daa8c0ef1fc0dac13a1e9be7ce056e4527017b0
weichen0218 <iamweichen0218@gmail.com>
2023-09-16 20:29:11 +0800

    add carousel

--- js/index.js
+++ js/index.js
@@ -1,62 +1,64 @@
 // Navbar animation && Button animation
 const navEl = document.querySelector("#navbar");
 const btnTop = document.getElementById("btn-back-to-top");
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
   if (window.scrollY >= 40) {
     navEl.classList.add("navbar-scrolled");
     btnTop.style.display = "block";
-  }else if (window.scrollY < 40){
+  } else if (window.scrollY < 40) {
     navEl.classList.remove("navbar-scrolled");
     btnTop.style.display = "none";
   }
-})
+});
 
 // Navbar click active
 const navLinkEls = document.querySelectorAll(".nav-link");
-navLinkEls.forEach(navLinkEl => {
-  navLinkEl.addEventListener('click', () => {
+navLinkEls.forEach((navLinkEl) => {
+  navLinkEl.addEventListener("click", () => {
     console.log(navLinkEl);
-    document.querySelector('.active')?.classList.remove('active');
-    navLinkEl.classList.add('active');
-  })
-})
+    document.querySelector(".active")?.classList.remove("active");
+    navLinkEl.classList.add("active");
+  });
+});
 
 // Navbar scroll active
-const sectionEls = document.querySelectorAll('section');
-let currentSection = 'home';
+const sectionEls = document.querySelectorAll("section");
+let currentSection = "home";
 window.addEventListener("scroll", () => {
-  sectionEls.forEach(sectionEl => {
-    if (window.scrollY >= sectionEl.offsetTop - 180){
+  sectionEls.forEach((sectionEl) => {
+    if (window.scrollY >= sectionEl.offsetTop - 180) {
       currentSection = sectionEl.id;
     }
-  })
+  });
 
-  navLinkEls.forEach(navLinkEl => {
-    if (navLinkEl.href.includes(currentSection)){
-      document.querySelector('.active')?.classList.remove('active');
-      navLinkEl.classList.add('active');
+  navLinkEls.forEach((navLinkEl) => {
+    if (navLinkEl.href.includes(currentSection)) {
+      document.querySelector(".active")?.classList.remove("active");
+      navLinkEl.classList.add("active");
     }
-  })
-})
+  });
+});
 
 // Fixed navigations covering content on scroll
-const navHeight = document.querySelector('#navbar').offsetHeight;
-document.documentElement.style.setProperty("--scroll-padding", navHeight-1 + "px")
-
+const navHeight = document.querySelector("#navbar").offsetHeight;
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navHeight - 1 + "px"
+);
 
 // Save form value
-const feedback =  document.querySelectorAll('input, textarea');
-feedback.forEach(element => {
-  element.addEventListener("keyup", function(){
+const feedback = document.querySelectorAll("input, textarea");
+feedback.forEach((element) => {
+  element.addEventListener("keyup", function () {
     sessionStorage.setItem(element.name, element.value);
-  })
-})
+  });
+});
 
 // Set form value
-document.addEventListener("DOMContentLoaded", function() {
-  feedback.forEach(element => {
+document.addEventListener("DOMContentLoaded", function () {
+  feedback.forEach((element) => {
     element.value = sessionStorage.getItem(element.name);
-  })
+  });
   // $('.card--float').hover(function () {
   //   $('.card--float').stop().fadeTo('fast', 0.3);
   //   $(this).stop().fadeTo('fast', 1);
